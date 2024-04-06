a6a2f6f836011cf5ef27cd4086ae9b8975b4becc
MarwaAM <marwamahmud4@gmail.com>
2024-04-07 01:51:12 +0200

    init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  useTabs: true,
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  endOfLine: "auto",
+};
